68d341375bcdf748232aaff2def627b890eec4db
Sid Roberts <sid@sidroberts.co.uk>
2021-08-31 21:51:50 +0900

    JS standard fixes.

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-var video = document.querySelector("#webcam")
+/* global $ */
+
+const video = document.querySelector('#webcam')
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
-      video.srcObject = stream;
+      video.srcObject = stream
     })
     .catch(function (e) {
-      console.log("Failed to display webcam.")
+      console.log('Failed to display webcam.')
     })
 } else {
-  console.log("Failed to find webcam.");
+  console.log('Failed to find webcam.')
 }
 
-$("#requestFullscreen").click(
+$('#requestFullscreen').click(
   function () {
-    video.requestFullscreen();
+    video.requestFullscreen()
   }
 )
